26452bc5c22c09785552730320eeecb599c30efc
Shuhad <shuhadz@hotmail.com>
2021-11-12 13:51:09 +0100

    api url updated

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ document.getElementById('connect').addEventListener('click', async function(){
 
 const readcommand = async () => {
     try {
-        const response = await fetch('https://yoursiteurl/api.php/readcommand',{
+        const response = await fetch('http://smartsensor.io/api/api.php/readcommand',{
             method: "GET"
           });
          const data = await response.json();
@@ -33,7 +33,7 @@ const stopProcess =()=>{
 }
 const writeResponse = async (res) => {
     try {
-        const response = await fetch('https://yoursiteurl/api.php/senddongleresponse', {
+        const response = await fetch('http://smartsensor.io/api/api.php/senddongleresponse', {
             method: 'POST',        
             body: JSON.stringify({
                 response: JSON.stringify(res)
